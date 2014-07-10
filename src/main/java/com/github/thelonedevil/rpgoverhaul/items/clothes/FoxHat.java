f92765e446e6596179ea3c718be501fd1700b032
Justin Wiblin <Justin_wiblin@hotmail.co.uk>
2014-07-10 03:52:02 +0100

    Testing CI
    Signed-off-by: Justin Wiblin <Justin_wiblin@hotmail.co.uk>

--- src/main/java/com/github/thelonedevil/rpgoverhaul/items/clothes/FoxHat.java
+++ src/main/java/com/github/thelonedevil/rpgoverhaul/items/clothes/FoxHat.java
@@ -9,21 +9,23 @@ import net.minecraft.item.ItemStack;
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
 
-public class FoxHat extends ItemArmor{
+public class FoxHat extends ItemArmor {
 
 	public FoxHat(ArmorMaterial p_i45325_1_, int p_i45325_2_, int p_i45325_3_) {
 		super(p_i45325_1_, p_i45325_2_, p_i45325_3_);
 		// TODO Auto-generated constructor stub
 	}
+
 	@SideOnly(Side.CLIENT)
-	public void registerIcons(IIconRegister icon){
-		this.itemIcon = icon.registerIcon(Ref.MODID+":foxHat");
+	public void registerIcons(IIconRegister icon) {
+		this.itemIcon = icon.registerIcon(Ref.MODID + ":foxHat");
 	}
 
-	public String getArmorTexture(ItemStack stack, Entity entity, int slot, String type){
+	public String getArmorTexture(ItemStack stack, Entity entity, int slot,
+			String type) {
 
-		return Ref.MODID+":textures/models/armor/fox_Layer1.png";
-		
+		return Ref.MODID + ":textures/models/armor/fox_Layer1.png";
+
 	}
 
 }
