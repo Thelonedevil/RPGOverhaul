5803900f26540771bce49d4bfa829ed69e498f16
Justin Wiblin <Justin_wiblin@hotmail.co.uk>
2014-07-13 12:45:54 +0100

    Gazelles now drop raw gazelle or cooked gazelle depending on burning status
    Signed-off-by: Justin Wiblin <Justin_wiblin@hotmail.co.uk>

--- src/main/java/com/github/thelonedevil/rpgoverhaul/mobs/passive/Gazelle.java
+++ src/main/java/com/github/thelonedevil/rpgoverhaul/mobs/passive/Gazelle.java
@@ -1,6 +1,9 @@
 package com.github.thelonedevil.rpgoverhaul.mobs.passive;
 
+import com.github.thelonedevil.rpgoverhaul.MyItems;
+
 import net.minecraft.entity.EntityAgeable;
+import net.minecraft.entity.SharedMonsterAttributes;
 import net.minecraft.entity.ai.EntityAIFollowParent;
 import net.minecraft.entity.ai.EntityAILookIdle;
 import net.minecraft.entity.ai.EntityAIMate;
@@ -13,6 +16,7 @@ import net.minecraft.entity.passive.EntityAnimal;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.Blocks;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
 import net.minecraft.world.World;
 
 public class Gazelle extends EntityAnimal {
@@ -36,6 +40,38 @@ public class Gazelle extends EntityAnimal {
 	public EntityAgeable createChild(EntityAgeable p_90011_1_) {
 		// TODO Auto-generated method stub
 		return new Gazelle(this.worldObj);
+	}
+	public boolean isAIEnabled() {
+		return true;
+	}
+
+	protected void applyEntityAttributes() {
+		super.applyEntityAttributes();
+		this.getEntityAttribute(SharedMonsterAttributes.maxHealth).setBaseValue(10.0D);
+		this.getEntityAttribute(SharedMonsterAttributes.movementSpeed).setBaseValue(0.25D);
+	}
+
+	protected void updateAITasks() {
+		super.updateAITasks();
+	}
+	protected Item getDropItem() {
+		return this.isBurning() ? MyItems.CookedGazelle : MyItems.RawGazelle;
+	}
+
+	protected void dropFewItems(boolean p_70628_1_, int p_70628_2_) {
+		int j = this.rand.nextInt(3) + this.rand.nextInt(1 + p_70628_2_);
+		int k;
+		for (k = 0; k < j; ++k) {
+			if (this.isBurning()) {
+				this.dropItem(MyItems.CookedGazelle, 1);
+			} else {
+				this.dropItem(MyItems.RawGazelle, 1);
+			}
+		}
+	}
+
+	public boolean isBreedingItem(ItemStack p_70877_1_) {
+		return p_70877_1_ != null && p_70877_1_.getItem() == Item.getItemFromBlock(Blocks.vine);
 	}
 
 }
