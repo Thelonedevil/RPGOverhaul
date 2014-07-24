6ba83c8fb6cce861abee9f5cf61530748aa14435
Justin Wiblin <Justin_wiblin@hotmail.co.uk>
2014-07-24 19:56:24 +0100

    Simplified Fist renderer
    Signed-off-by: Justin Wiblin <Justin_wiblin@hotmail.co.uk>

--- src/main/java/com/github/thelonedevil/rpgoverhaul/renderer/item/FistRenderer.java
+++ src/main/java/com/github/thelonedevil/rpgoverhaul/renderer/item/FistRenderer.java
@@ -1,21 +1,16 @@
 package com.github.thelonedevil.rpgoverhaul.renderer.item;
 
-import com.github.thelonedevil.rpgoverhaul.models.ModelArm;
 import net.minecraft.client.Minecraft;
-import net.minecraft.client.entity.EntityPlayerSP;
 import net.minecraft.client.renderer.entity.Render;
 import net.minecraft.client.renderer.entity.RenderManager;
 import net.minecraft.client.renderer.entity.RenderPlayer;
-import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
 import net.minecraftforge.client.IItemRenderer;
 import org.lwjgl.opengl.GL11;
 
 public class FistRenderer implements IItemRenderer {
-    ModelArm modelArm;
-
     public FistRenderer() {
-        modelArm = new ModelArm();
+
     }
 
     @Override
@@ -35,42 +30,22 @@ public class FistRenderer implements IItemRenderer {
         Minecraft mc = Minecraft.getMinecraft();
         switch (type) {
             case EQUIPPED:
+
                 break;
             case EQUIPPED_FIRST_PERSON:
-                float var7 = 0.8F;
                 GL11.glPushMatrix();
                 if (mc.gameSettings.thirdPersonView == 0) {
-                    GL11.glTranslatef(1.0F, 0.75F, -1.0F);
-                    GL11.glRotatef(135.0F, 0.0F, -1.0F, 0.0F);
-
-                    GL11.glTranslatef(-0.7F * var7, -(-0.65F * var7) + (1.0F) * 1.5F, 0.9F * var7);
-                    GL11.glRotatef(90.0F, 0.0F, 1.0F, 0.0F);
-                    GL11.glTranslatef(0.0F, 0.0F * var7, -0.9F * var7);
-                    GL11.glRotatef(90.0F, 0.0F, 1.0F, 0.0F);
-                    GL11.glRotatef(0.0F, 0.0F, 0.0F, 1.0F);
-                    GL11.glEnable(32826);
-                    GL11.glPushMatrix();
-                    GL11.glScalef(5.0F, 5.0F, 5.0F);
                     mc.renderEngine.bindTexture(mc.thePlayer.getLocationSkin());
-                    for (int var9 = 1; var9 < 2; var9++) {
-                        GL11.glPushMatrix();
-                        GL11.glTranslatef(-0.0F, -1.4F, 1.1F );
-                        GL11.glRotatef(15.0f, 1.0F, 0.0F, 0.0F);
-                        GL11.glRotatef(-90.0F, 0.0F, 0.0F, 1.0F);
-                        GL11.glRotatef(59.0F, 0.0F, 0.0F, 1.0F);
-                        GL11.glRotatef(-65 , 0.0F, 1.0F, 0.0F);
-                        Render var24 = RenderManager.instance.getEntityRenderObject(mc.thePlayer);
-                        RenderPlayer var26 = (RenderPlayer) var24;
-                        float var13 = 1.0F;
-                        GL11.glScalef(var13, var13, var13);
-                        var26.renderFirstPersonArm(mc.thePlayer);
-                        GL11.glPopMatrix();
-                    }
-                    GL11.glPopMatrix();
-                    GL11.glTranslatef(0.4F, -0.4F, 0.0F);
-                    GL11.glEnable(32826);
-                    GL11.glScalef(2.0F, 2.0F, 2.0F);
+                    GL11.glRotatef(135.0F, 0.0F, 1.0F, 0.0F);
+                    GL11.glRotatef(45.0f, 1.0F, 0.0F, 0.0F);
+                    GL11.glTranslatef(0F, -0.7F, 0F);
+                    GL11.glScalef(2.5F, 2.5F, 2.5F);
+                    Render render = RenderManager.instance.getEntityRenderObject(mc.thePlayer);
+                    RenderPlayer player = (RenderPlayer) render;
+                    player.renderFirstPersonArm(mc.thePlayer);
                 }
+                GL11.glPopMatrix();
+
                 GL11.glPopMatrix();
                 break;
             case ENTITY:
