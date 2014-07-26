9b77fc8454dd53f6f64cb4817f79f07e92cd7f73
Justin Wiblin <Justin_wiblin@hotmail.co.uk>
2014-07-26 22:08:33 +0100

    im a mister herp a derp today, added legendary hammer framework...
    Signed-off-by: Justin Wiblin <Justin_wiblin@hotmail.co.uk>

--- src/main/java/com/github/thelonedevil/rpgoverhaul/weapons/hammer/Legendary/LegendaryHammer.java
+++ src/main/java/com/github/thelonedevil/rpgoverhaul/weapons/hammer/Legendary/LegendaryHammer.java
@@ -1,9 +1,29 @@
 package com.github.thelonedevil.rpgoverhaul.weapons.hammer.Legendary;
 
+import com.github.thelonedevil.rpgoverhaul.Ref;
 import com.github.thelonedevil.rpgoverhaul.weapons.hammer.Hammer;
+import net.minecraft.item.EnumRarity;
+import net.minecraft.item.ItemStack;
 
 /**
  * Created by justin on 26/07/2014.
  */
 public class LegendaryHammer extends Hammer {
+    String lore;
+
+    public LegendaryHammer(String name, String lore, int[] stats) {
+        super(name, stats);
+        this.lore = lore;
+        this.setTextureName(Ref.MODID + ":" + name);
+    }
+
+    public String getLore() {
+        return lore;
+    }
+
+    @Override
+    public EnumRarity getRarity(ItemStack par1ItemStack) {
+        return EnumRarity.epic;
+
+    }
 }
