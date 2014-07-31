6ec02a637830f2c254852617946a2a1ed5e49be1
Justin Wiblin <Justin_wiblin@hotmail.co.uk>
2014-07-31 18:08:27 +0100

    Ghasts and the ender dragon now have dynamic health, the health increase system will be changing soon, need to work out a good balance
    Signed-off-by: Justin Wiblin <Justin_wiblin@hotmail.co.uk>

--- src/main/java/com/github/thelonedevil/rpgoverhaul/handlers/EntityJoinWorldHandler.java
+++ src/main/java/com/github/thelonedevil/rpgoverhaul/handlers/EntityJoinWorldHandler.java
@@ -6,6 +6,8 @@ import com.github.thelonedevil.rpgoverhaul.network.SyncEEP;
 import com.github.thelonedevil.rpgoverhaul.player.ExtendedPlayer;
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
 import net.minecraft.entity.SharedMonsterAttributes;
+import net.minecraft.entity.boss.EntityDragon;
+import net.minecraft.entity.monster.EntityGhast;
 import net.minecraft.entity.monster.EntityMob;
 import net.minecraft.entity.monster.EntitySlime;
 import net.minecraft.entity.player.EntityPlayer;
@@ -29,8 +31,8 @@ public class EntityJoinWorldHandler {
                     int level1 = level + 3;
                     mob.setLevel(level1);
                     double health = ((EntityMob) event.entity).getEntityAttribute(SharedMonsterAttributes.maxHealth).getBaseValue();
-                    ((EntityMob) event.entity).getEntityAttribute(SharedMonsterAttributes.maxHealth).setBaseValue((level1 * 0.8) * 3 + health);
-                    ((EntityMob) event.entity).setHealth((float) ((level1 * 0.8) * 3 + health));
+                    ((EntityMob) event.entity).getEntityAttribute(SharedMonsterAttributes.maxHealth).setBaseValue((level1 * 0.8) * 3 + (health *0.8));
+                    ((EntityMob) event.entity).setHealth((float) ((level1 * 0.8) * 3 + (health *0.8)));
                     double damage = ((EntityMob) event.entity).getEntityAttribute(SharedMonsterAttributes.attackDamage).getBaseValue();
                     ((EntityMob) event.entity).getEntityAttribute(SharedMonsterAttributes.attackDamage).setBaseValue((level1 * 0.8) * 3 + damage);
                 } else {
@@ -46,8 +48,36 @@ public class EntityJoinWorldHandler {
                     mob.setLevel(level1);
                     double health = ((EntitySlime) event.entity).getEntityAttribute(SharedMonsterAttributes.maxHealth).getBaseValue();
                     int size = ((EntitySlime) event.entity).getSlimeSize() * 6;
-                    ((EntitySlime) event.entity).getEntityAttribute(SharedMonsterAttributes.maxHealth).setBaseValue((level1 * 0.8) * 3 +health * size);
-                    ((EntitySlime) event.entity).setHealth((float) ((level1 * 0.8) * 3+ health* size));
+                    ((EntitySlime) event.entity).getEntityAttribute(SharedMonsterAttributes.maxHealth).setBaseValue((level1 * 0.8) * 3 + (health *0.8));
+                    ((EntitySlime) event.entity).setHealth((float) ((level1 * 0.8) * 3 + (health *0.8)));
+                } else {
+                    mob.setLevel(3);
+                }
+            }
+            if (event.entity instanceof EntityGhast) {
+                ExtendedMob mob = ExtendedMob.get(event.entity);
+                EntityPlayer player = event.entity.worldObj.getClosestPlayerToEntity(event.entity, 128D);
+                if (player != null) {
+                    int level = ExtendedPlayer.get(player).getLevel();
+                    int level1 = level + 3;
+                    mob.setLevel(level1);
+                    double health = ((EntityGhast) event.entity).getEntityAttribute(SharedMonsterAttributes.maxHealth).getBaseValue();
+                    ((EntityGhast) event.entity).getEntityAttribute(SharedMonsterAttributes.maxHealth).setBaseValue((level1 * 0.8) * 3 + (health *0.8));
+                    ((EntityGhast) event.entity).setHealth((float) ((level1 * 0.8) * 3 + (health *0.8)));
+                } else {
+                    mob.setLevel(3);
+                }
+            }
+            if(event.entity instanceof EntityDragon){
+                ExtendedMob mob = ExtendedMob.get(event.entity);
+                EntityPlayer player = event.entity.worldObj.getClosestPlayerToEntity(event.entity, 128D);
+                if (player != null) {
+                    int level = ExtendedPlayer.get(player).getLevel();
+                    int level1 = level + 3;
+                    mob.setLevel(level1);
+                    double health = ((EntityDragon) event.entity).getEntityAttribute(SharedMonsterAttributes.maxHealth).getBaseValue();
+                    ((EntityDragon) event.entity).getEntityAttribute(SharedMonsterAttributes.maxHealth).setBaseValue((level1 * 0.8) * 3 + (health *0.8));
+                    ((EntityDragon) event.entity).setHealth((float) ((level1 * 0.8) * 3 + (health * 0.8)));
                 } else {
                     mob.setLevel(3);
                 }
